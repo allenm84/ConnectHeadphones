d539e493fa4576033ee00042d3e51969020eff87
Michael Allen <michael.allen@cognex.com>
2016-06-28 13:40:00 -0500

    [mod] Specify that this app is added to the PATH env var [mod] Add code

---
+++ src/ConnectHeadphones/Bluetooth.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using InTheHand.Net;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
+
+namespace ConnectHeadphones
+{
+  public static class Bluetooth
+  {
+    public static async Task<BluetoothDeviceInfo[]> Scan()
+    {
+      return await Task.Run(() =>
+      {
+        using (var localClient = new BluetoothClient())
+        {
+          return localClient.DiscoverDevices();
+        }
+      });
+    }
+
+    public static bool Connect(string address)
+    {
+      return Connect(BluetoothAddress.Parse(address));
+    }
+
+    public static bool Connect(BluetoothAddress address)
+    {
+      using (var localClient = new BluetoothClient())
+      {
+        localClient.Connect(address, BluetoothService.HeadsetHeadset);
+        return new BluetoothDeviceInfo(address).Connected;
+      }
+    }
+  }
+}
---
+++ src/ConnectHeadphones/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConnectHeadphones
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      bool succeed = false;
+      for (int attempts = 0; !succeed && attempts < 10; ++attempts)
+      {
+        try 
+        { 
+          Bluetooth.Connect("D09C3001A200");
+          succeed = true;
+          Console.WriteLine("Success!");
+        }
+        catch (Exception ex)
+        {
+          succeed = false;
+          Console.WriteLine("Unable to connect because {0}. Attempt {1}/10",
+            ex.Message, attempts + 1);
+          Console.WriteLine();
+          Thread.Sleep(500);
+        }
+      }
+    }
+  }
+}
